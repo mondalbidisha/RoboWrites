c9736935e68cc72516866bfd645c5c64e3658789
Bidisha Mondal <mondal.bidisha@gmail.com>
2024-06-12 19:18:31 +0530

    db seeding and prisma migration

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,4 @@
 export const GITHUB_CONTRIBUTOR_URL = '';
 export const BACKEND_URL = "http://localhost:8787";
 export const FF_ENABLE_AI = true; 
-export const FF_IMAGE_UPLOADS = true; 
+export const FF_IMAGE_UPLOADS = false; 
