82158e4a8424846e91a75257dd2d6a5fcff05dd7
Bidisha Mondal <mondal.bidisha@gmail.com>
2024-06-30 23:52:00 +0530

    config changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,5 @@
 export const GITHUB_CONTRIBUTOR_URL = '';
 export const BACKEND_URL = "https://backend.mondal-bidisha.workers.dev";
+// export const BACKEND_URL_DEV = "http://localhost:8787";
 export const FF_ENABLE_AI = true; 
 export const FF_IMAGE_UPLOADS = false; 
